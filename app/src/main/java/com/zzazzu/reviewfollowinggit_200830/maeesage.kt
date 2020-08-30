bd397118a568b03af4c29187efe37435bdd58a62
bucephalus41 <bucephalus41@gmail.com>
2020-08-30 12:48:11 +0900

    화면 다 만듦

---
+++ app/src/main/java/com/zzazzu/reviewfollowinggit_200830/maeesage.kt
@@ -0,0 +1,11 @@
+package com.zzazzu.reviewfollowinggit_200830
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class maeesage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maeesage)
+    }
+}
---
+++ app/src/main/java/com/zzazzu/reviewfollowinggit_200830/other.kt
@@ -0,0 +1,11 @@
+package com.zzazzu.reviewfollowinggit_200830
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class other : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+}
